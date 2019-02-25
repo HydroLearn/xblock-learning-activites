98e6b42ad6eda08c55985f3f5ff9efbbb38fde2f
crivet <c.rivet@louisiana.edu>
2019-02-25 15:24:01 -0600

    forgot a save

--- hl_learning_activities/static/js/learning_activity_script.js
+++ hl_learning_activities/static/js/learning_activity_script.js
@@ -1,4 +1,4 @@
-function HLCK5_XBlockStudio(runtime, xblock_element) {
+function Learning_Activity_Studio(runtime, xblock_element) {
 
     $(function($) {
         console.log('Initialized custom LA script.')
