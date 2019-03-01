7c5a723448f2a79d29eb331b20fe5e0e380d5bec
crivet <c.rivet@louisiana.edu>
2019-03-01 10:44:48 -0600

    restructuring to make Learning Activity be it's own unique studio container block to contain additional xblocks

--- hl_learning_activities/hl_learning_activities.py
+++ hl_learning_activities/hl_learning_activities.py
@@ -32,6 +32,12 @@ from xblock.fields import (
         ReferenceList, # list of references to other xblocks
     )
 
+# including nested xblock container
+#       source references for these:
+#           https://github.com/edx/xblock-utils/blob/master/xblockutils/studio_editable.py
+#
+from xblockutils.studio_editable import StudioContainerXBlockMixin
+from xblockutils.studio_editable import StudioEditableXBlockMixin
 
 # from xblock.fragment import Fragment #DEPRECIATED
 from web_fragments.fragment import Fragment
@@ -40,7 +46,7 @@ from web_fragments.fragment import Fragment
 from hl_text import hl_text_XBlock
 
 
-class HL_LearningActivity_XBlock(hl_text_XBlock):
+class HL_LearningActivity_XBlock(StudioContainerXBlockMixin, XBlock):
 
     # modify path to the custom starter template for empty xblocks
     #empty_template = 'templates/initial_learning_activity_template.html'
@@ -57,18 +63,67 @@ class HL_LearningActivity_XBlock(hl_text_XBlock):
 
     def studio_view(self, context):
 
-        fragment = super(HL_LearningActivity_XBlock, self).studio_view(context)
+        fragment = Fragment()
 
-        fragment.add_css(load_resource('static/css/learning_activity_styling.css'))
-        fragment.add_javascript(load_resource('static/js/learning_activity_script.js'))
+        fragment.add_content(render_template('templates/hl_learning_activity-cms.html', context))
+
+        fragment.add_css(load_resource('static/css/hl_learning_activity-cms.css'))
+        fragment.add_javascript(load_resource('static/js/hl_learning_activity-cms.js'))
         fragment.initialize_js('Learning_Activity_Studio')
 
         return fragment
 
-
     def student_view(self, context):
 
-        fragment = super(HL_LearningActivity_XBlock, self).student_view(context)
+        fragment = Fragment()
 
+        fragment.add_css(load_resource('static/css/hl_learning_activity-lms.css'))
+        fragment.add_content(render_template('templates/hl_learning_activity-lms.html', content))
+        fragment.add_javascript(load_resource('static/js/hl_learning_activity-lms.js'))
+        fragment.initialize_js('Learning_Activity')
 
         return fragment
+
+
+    def author_edit_view(self, context):
+        """
+        Child blocks can override this to control the view shown to authors in Studio when
+        editing this block's children.
+        """
+        fragment = Fragment()
+        self.render_children(context, fragment, can_reorder=True, can_add=True)
+        return fragment
+
+
+# origional implementation as just a text block
+# class HL_LearningActivity_XBlock(hl_text_XBlock):
+#
+#     # modify path to the custom starter template for empty xblocks
+#     #empty_template = 'templates/initial_learning_activity_template.html'
+#
+#     display_name = String(
+#         display_name="Learning Activity",
+#         help="This name appears in the horizontal navigation at the top of the page",
+#         scope=Scope.settings,
+#         default="Learning Activity"
+#     )
+#
+#     def get_empty_template(self, context={}):
+#         return render_template('templates/initial_learning_activity_template.html', context)
+#
+#     def studio_view(self, context):
+#
+#         fragment = super(HL_LearningActivity_XBlock, self).studio_view(context)
+#
+#         fragment.add_css(load_resource('static/css/learning_activity_styling.css'))
+#         fragment.add_javascript(load_resource('static/js/learning_activity_script.js'))
+#         fragment.initialize_js('Learning_Activity_Studio')
+#
+#         return fragment
+#
+#     def student_view(self, context):
+#
+#         fragment = super(HL_LearningActivity_XBlock, self).student_view(context)
+#
+#
+#         return fragment
---
+++ hl_learning_activities/static/js/hl_learning_activity-lms.js
@@ -0,0 +1,11 @@
+function Learning_Activity(runtime, xblock_element) {
+
+    // extend from the default HLCK5 studio object
+    // HL_TEXT_STUDIO.call(this, runtime, xblock_element);
+
+
+    $(function($) {
+        console.log('Initialized custom LA lms script.')
+
+    })
+}
